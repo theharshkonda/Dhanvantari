73713a0f8e858b08900a4ada9298e7c10f47993e
theharshkonda <theharshkonda@gmail.com>
2024-01-23 10:19:07 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import AppNavigator from './src/navigation/AppNavigator';
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <AppNavigator/>
+  );
+};
+
+export default App;
